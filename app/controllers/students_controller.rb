95e8f187dc2cf1c9a4d3a3b882fad5f76127a04b
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-10 11:20:45 -0600

    EDD Rails dynamic requestpractice

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @students = Student.create_with(first_name: nil , last_name: nil )
+    @student = Student.find(params[:id])
+    render :show 
+  end
 end
